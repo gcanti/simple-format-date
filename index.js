1ee0d8e97ad4903005b5dd6e4e726f5843868b37
gcanti <giulio.canti@gmail.com>
2016-01-03 09:43:35 +0100

    use lodash padLeft

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 var template = require('lodash/string/template');
+var padLeft = require('lodash/string/padLeft');
 
 var templates = {};
-
-function padLeft(x, len) {
-  var s = x + '';
-  var times = len - s.length;
-  for (var i = 0 ; i < times ; i++ ) {
-    s = '0' + s;
-  }
-  return s;
-}
 
 function splitDate(date) {
   var YY = date.getFullYear();
@@ -22,15 +14,15 @@ function splitDate(date) {
     Y: String(YY).substr(-2),   // short year (15 for 2015)
     YY: YY,                     // numeric long year
     M: M,                       // numeric month (9 for September)
-    MM: padLeft(M, 2),          // padded month ('09' for September)
+    MM: padLeft(M, 2, '0'),     // padded month ('09' for September)
     D: D,                       // numeric day
-    DD: padLeft(D, 2),          // padded day
+    DD: padLeft(D, 2, '0'),     // padded day
     h: h,                       // numeric hours
-    hh: padLeft(h, 2),          // padded hours
+    hh: padLeft(h, 2, '0'),     // padded hours
     m: m,                       // numeric minutes
-    mm: padLeft(m, 2),          // padded minutes
+    mm: padLeft(m, 2, '0'),     // padded minutes
     s: s,                       // numeric secs
-    ss: padLeft(s, 2)           // padded secs
+    ss: padLeft(s, 2, '0')      // padded secs
   };
 }
 
